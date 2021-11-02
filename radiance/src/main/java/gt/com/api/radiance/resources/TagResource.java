10c68f5207d7de4be6b37aa32ddda15216e1b76c
Marvin <malopez@takitosu.localdomain>
2021-11-01 22:50:59 -0600

    Cambios en el get general
    Se agrego verificacion de token
    Se elimino la paginacion

--- radiance/src/main/java/gt/com/api/radiance/resources/TagResource.java
+++ radiance/src/main/java/gt/com/api/radiance/resources/TagResource.java
@@ -8,10 +8,12 @@ package gt.com.api.radiance.resources;
 
 import gt.com.api.radiance.controllers.TagController;
 import gt.com.api.radiance.dtos.TagModel;
-import gt.com.api.radiance.dtos.TagPage;
+import gt.com.api.radiance.dtos.UserLoad;
 import gt.com.api.radiance.verify.ApiVersionValidator;
+import gt.com.api.radiance.verify.Authenticator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -45,20 +47,19 @@ public class TagResource {
 
     @ApiOperation(value = "Get a tag list", notes = "Get a list of tags")
     @GET
-    public TagPage getTags(@QueryParam("page") @DefaultValue("1") Long page,
-            @QueryParam("size") @DefaultValue("20") Long size, @QueryParam("filter") @DefaultValue("") String filter,
+    public List<TagModel> getTags(@QueryParam("filter") @DefaultValue("") String filter,
             @Context HttpServletRequest request) {
         long startTime = System.currentTimeMillis();
         ApiVersionValidator.validate(request);
-//        UserLoad userLoad = Authenticator.tokenValidation(request);
-        TagPage tags = TAG_CONTROLLER.getTagPage(size, page, filter);
+        UserLoad userLoad = Authenticator.tokenValidation(request);
+        List<TagModel> tags = TAG_CONTROLLER.getTags(filter);
         if (tags == null) {
             LOGGER.error("Time of not GET tag list: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST);
+                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST + " " + userLoad.toString());
             throw new WebApplicationException("Cannot get tag list ", Response.Status.BAD_REQUEST);
         }
         LOGGER.info("Time to GET tag list: " + (System.currentTimeMillis() - startTime)
-                + " milliseconds, statusCode:" + Response.Status.OK);
+                + " milliseconds, statusCode:" + Response.Status.OK + " " + userLoad.toString());
         return tags;
     }
 
@@ -68,15 +69,15 @@ public class TagResource {
     public TagModel getTag(@PathParam("id") String id, @Context HttpServletRequest request) {
         long startTime = System.currentTimeMillis();
         ApiVersionValidator.validate(request);
-//        UserLoad userLoad = Authenticator.tokenValidation(request);
+        UserLoad userLoad = Authenticator.tokenValidation(request);
         TagModel tag = TAG_CONTROLLER.getTag(id);
         if (tag == null) {
             LOGGER.error("Time of not GET tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST);
+                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST + " " + userLoad.toString());
             throw new WebApplicationException("Cannot get tag ", Response.Status.BAD_REQUEST);
         }
         LOGGER.info("Time to GET tag: " + (System.currentTimeMillis() - startTime)
-                + " milliseconds, statusCode:" + Response.Status.OK);
+                + " milliseconds, statusCode:" + Response.Status.OK + " " + userLoad.toString());
         return tag;
     }
 
@@ -85,21 +86,22 @@ public class TagResource {
     public TagModel postTag(TagModel tag, @Context HttpServletRequest request) {
         long startTime = System.currentTimeMillis();
         ApiVersionValidator.validate(request);
-//        UserLoad userLoad = Authenticator.tokenValidation(request);
+        UserLoad userLoad = Authenticator.tokenValidation(request);
         //verification of required fields
         if (tag.getName().equals("") || tag.getColor().equals("") || tag.getIcon().equals("")) {
             LOGGER.error("Time of not save tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.NOT_ACCEPTABLE.getStatusCode());
+                    + " milliseconds, statusCode:" + Response.Status.NOT_ACCEPTABLE.getStatusCode()
+                    + " " + userLoad.toString());
             throw new WebApplicationException("Fields are missing ", Response.Status.NOT_ACCEPTABLE);
         }
         TagModel newTag = TAG_CONTROLLER.saveTag(tag);
         if (newTag == null) {
             LOGGER.error("Time of not save new tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST);
+                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST + " " + userLoad.toString());
             throw new WebApplicationException("Cannot post tag ", Response.Status.BAD_REQUEST);
         }
         LOGGER.info("Time to POST tag: " + (System.currentTimeMillis() - startTime)
-                + " milliseconds, statusCode:" + Response.Status.OK);
+                + " milliseconds, statusCode:" + Response.Status.OK + " " + userLoad.toString());
         return newTag;
     }
 
@@ -109,28 +111,30 @@ public class TagResource {
     public TagModel putTag(TagModel tag, @PathParam("id") String id, @Context HttpServletRequest request) {
         long startTime = System.currentTimeMillis();
         ApiVersionValidator.validate(request);
-//        UserLoad userLoad = Authenticator.tokenValidation(request);
+        UserLoad userLoad = Authenticator.tokenValidation(request);
         //verification of required fields
         if (tag.getName().equals("") || tag.getColor().equals("") || tag.getIcon().equals("")) {
             LOGGER.error("Time of not save tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.NOT_ACCEPTABLE.getStatusCode());
+                    + " milliseconds, statusCode:" + Response.Status.NOT_ACCEPTABLE.getStatusCode()
+                    + " " + userLoad.toString());
             throw new WebApplicationException("Fields are missing ", Response.Status.NOT_ACCEPTABLE);
         }
         //verificate tag exists
         if (!TAG_CONTROLLER.verifyTagExists(id)) {
             LOGGER.error("Time of not update tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.NOT_FOUND.getStatusCode());
+                    + " milliseconds, statusCode:" + Response.Status.NOT_FOUND.getStatusCode()
+                    + " " + userLoad.toString());
             throw new WebApplicationException("Tag not found, tagID: " + id,
                     Response.Status.NOT_FOUND);
         }
         TagModel updateTag = TAG_CONTROLLER.updateTag(id, tag);
         if (updateTag == null) {
             LOGGER.error("Time of not update tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST);
+                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST + " " + userLoad.toString());
             throw new WebApplicationException("Cannot put subscription type ", Response.Status.BAD_REQUEST);
         }
         LOGGER.info("Time to PUT tag: " + (System.currentTimeMillis() - startTime)
-                + " milliseconds, statusCode:" + Response.Status.OK);
+                + " milliseconds, statusCode:" + Response.Status.OK + " " + userLoad.toString());
         return updateTag;
     }
 
@@ -140,21 +144,22 @@ public class TagResource {
     public Response deleteTag(@PathParam("id") String id, @Context HttpServletRequest request) {
         long startTime = System.currentTimeMillis();
         ApiVersionValidator.validate(request);
-//        UserLoad userLoad = Authenticator.tokenValidation(request);
+        UserLoad userLoad = Authenticator.tokenValidation(request);
         //verificate tag exists
         if (!TAG_CONTROLLER.verifyTagExists(id)) {
             LOGGER.error("Time of not delete tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.NOT_FOUND.getStatusCode());
+                    + " milliseconds, statusCode:" + Response.Status.NOT_FOUND.getStatusCode()
+                    + " " + userLoad.toString());
             throw new WebApplicationException("Tag not found, tagID: " + id,
                     Response.Status.NOT_FOUND);
         }
         if (!TAG_CONTROLLER.deleteTag(id)) {
             LOGGER.error("Time of not delete tag: " + (System.currentTimeMillis() - startTime)
-                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST);
+                    + " milliseconds, statusCode:" + Response.Status.BAD_REQUEST + " " + userLoad.toString());
             throw new WebApplicationException("Cannot delete tag ", Response.Status.BAD_REQUEST);
         }
         LOGGER.info("Time to DELETE tag: " + (System.currentTimeMillis() - startTime)
-                + " milliseconds, statusCode:" + Response.Status.OK);
+                + " milliseconds, statusCode:" + Response.Status.OK + " " + userLoad.toString());
         return Response.status(Response.Status.OK).entity("OK").build();
     }
 
